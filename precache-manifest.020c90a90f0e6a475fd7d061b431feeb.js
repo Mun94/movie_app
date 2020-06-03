8e6b62605573908bd7a6d438ae15cca3b69abf61
Mun Seong Woo <seongwoo720@gmail.com>
2020-06-03 18:30:04 +0900

    Updates

---
+++ precache-manifest.020c90a90f0e6a475fd7d061b431feeb.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "60d466b87f6da2dc8d80d66c918f33ad",
+    "url": "/movie_app/index.html"
+  },
+  {
+    "revision": "046c6a68b71594ef6fc9",
+    "url": "/movie_app/static/css/main.a39944f6.chunk.css"
+  },
+  {
+    "revision": "ce65ecbbe798365775a9",
+    "url": "/movie_app/static/js/2.1347d8ad.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/movie_app/static/js/2.1347d8ad.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "046c6a68b71594ef6fc9",
+    "url": "/movie_app/static/js/main.6e04b098.chunk.js"
+  },
+  {
+    "revision": "793037b19926e1970ea2",
+    "url": "/movie_app/static/js/runtime-main.8469f341.js"
+  }
+]);
