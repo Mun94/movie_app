6318dbcfedea4747da3ada367439fe5904adf849
Mun94 <seongwoo720@gmail.com>
2020-06-03 14:54:37 +0900

    Styling the loading screen

--- src/App.js
+++ src/App.js
@@ -1,5 +1,7 @@
 import React from 'react';
 import axios from "axios";
+import Movie from "./Movie.js";
+import "./App.css";
 
 class App extends React.Component{
   state = {
@@ -8,7 +10,8 @@ class App extends React.Component{
   };
 
   getMovies = async() => {
-    const movies = await axios.get("https://yts-proxy.now.sh/list_movies.json");
+    const {data:{data:{movies}}} = await axios.get("https://yts-proxy.now.sh/list_movies.json?sort_by=rating");//movies.data.data.movies
+    this.setState({movies, isLoading:false});
   };
 
   componentDidMount(){
@@ -16,11 +19,38 @@ class App extends React.Component{
   };
 
   render(){
-    const {isLoading} = this.state;
-    return <div>
-        {isLoading ? "Loding..." : "we are ready"} 
-      </div>;
-    };
+    const {isLoading, movies} = this.state;
+    return (
+      <section className ="container">
+        {isLoading ? (
+        <div className = "loader">
+          <span className = "loader__text">
+            <span></span>
+            <span></span>
+            <span></span>
+            <span></span>
+            loading..
+          </span>
+        </div>
+        ) : (
+          <div className = "movies">
+              {movies.map(movie => (
+                <Movie
+                  key={movie.id}
+                  id={movie.id}
+                  year={movie.year}
+                  title={movie.title}
+                  summary={movie.summary}
+                  poster={movie.medium_cover_image}
+                  genres={movie.genres}
+                />
+          ))}
+          </div>
+          )
+        } 
+      </section>
+    );
+  };
 }
 
 export default App;
