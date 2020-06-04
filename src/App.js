cf45676afc546294bea0558decc173808dc1f6ab
Mun Seong Woo <seongwoo720@gmail.com>
2020-06-04 13:37:24 +0900

    Navigation(home, about) 생성 및 styling

--- src/App.js
+++ src/App.js
@@ -1,7 +1,18 @@
 import React from "react";
+import {HashRouter, Route} from "react-router-dom";
+import Home from "./routes/Home.js";
+import About from "./routes/About.js";
+import Navigation from "./components/Navigation.js";
+import "./App.css";
 
 function App(){
-    return <span>something</span>
+    return (
+        <HashRouter>
+            <Navigation/>
+            <Route path = "/" exact={true} component = {Home}/>
+            <Route path = "/about" component = {About} />
+        </HashRouter>
+    )
 }
 
 export default App;
---
+++ src/routes/About.js
@@ -0,0 +1,16 @@
+import React from "react";
+import "./About.css";
+
+function About(){
+    return (
+        <div className = "about__container">
+            <sapn>
+                "Lorem ipsum dolor sit, amet consectetur adipisicing elit. Amet, quibusdam reprehenderit in incidunt fugit neque rerum aut reiciendis sit, provident esse obcaecati! Accusamus soluta vitae perspiciatis repudiandae debitis officia laudantium."
+            </sapn>
+
+            <span> - Lorem 2020</span>
+        </div>
+    );
+}
+
+export default About;
